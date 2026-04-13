de0e713cf7226f01ff12ac2bcbba98259c64b03a
Thextender42 <alanc8327@gmail.com>
2026-04-13 05:50:41 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("whatsappBtn").addEventListener("click", function() {
   // Substitua pelo número do pedreiro
   const phoneNumber = "5519983904646"; 
-  const message = "Olá, estou interessado em seus serviços de pedreiro. Poderianos conversar ?";
+  const message = "Olá, estou interessado em seus serviços. Poderianos conversar ?";
   const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
   window.open(url, "_blank");
 });
