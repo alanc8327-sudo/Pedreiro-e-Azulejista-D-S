85f3d5699148ef33912c223f7a171d78f70fd8dd
Thextender42 <alanc8327@gmail.com>
2026-04-15 19:03:51 -0300

    Replace WhatsApp button with feedback functions

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-document.getElementById("whatsappBtn").addEventListener("click", function() {
-  // Substitua pelo número do pedreiro
-  const phoneNumber = "5519983904646"; 
-  const message = "Olá, estou interessado em seus serviços. Poderianos conversar ?";
-  const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-  window.open(url, "_blank");
-});
+function enviarFeedback() {
+   let nome = document.getElementById("feedbackNome").value;
+   let orcamento = document.getElementById("feedbackOrcamento").value;
+   let endereco = document.getElementById("feedbackEndereco").value;
+
+   // Monta a mensagem
+   let mensagem = `Olá tudo bem? Sou o ${nome}, e gostaria de solicitar um orçamento para os serviços de pedreiro e azulejista.\n\n` +
+                  `Orçamento: ${orcamento}\n` +
+                  `Endereço: ${endereco}`;
+
+   // Número do WhatsApp no formato internacional (ex: Brasil 55 + DDD + número)
+   let numero = "5519983904646"; 
+   
+
+   // Cria a URL
+   let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+
+
+   // Abre o WhatsApp com a mensagem pronta
+   window.open(url, "_blank");
+  
+}
+
+function enviarFeedback2() {
+   let nome = document.getElementById("feedbackNome").value;
+   let orcamento = document.getElementById("feedbackOrcamento").value;
+   let endereco = document.getElementById("feedbackEndereco").value;
+
+    // Monta a mensagem 
+    let mensagem = `Ola tudo bem? Sou o ${nome}, e gostaria de solicitar um orçamento para os serviços de pedreiro e azulejista.\n\n` + 
+                    `Orçamento: ${orcamento}\n` +
+                    `Endereço: ${endereco}`;
+    // Número do WhatsApp no formato internacional (ex: Brasil 55 + DDD + número)
+    let numero = "5519987455667";
+    let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+    // Abre o WhatsApp com a mensagem pronta
+    window.open(url, "_blank");
+}
